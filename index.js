b1e3a10008ea5f051b5528ab9f287d4b6e51c504
Eddy <eddyjohn.terosier@hotmail.com>
2022-12-08 18:26:48 +0100

    Returning async function

--- index.js
+++ index.js
@@ -23,18 +23,43 @@ const getDogPic = async () => {
     try {
         const data = await readFilePro(`${__dirname}/dog.txt`);
         console.log(`Breed: ${data}`);
-    
+
         const res = await superagent.get(
             `https://dog.ceo/api/breed/${data}/images/random`
         );
         console.log(res.body.message);
         await writeFilePro("dog-image.txt", res.body.message);
         console.log("Random dog image saved");
-    } catch(err) {
+    } catch (err) {
         console.log(err);
+
+        throw err;
     }
+    return "2: READY";
 };
-getDogPic();
+
+(async()=>{
+    try{
+        console.log("1: Will get dog pics!");
+        const x = await getDogPic();
+        console.log(x);
+        console.log("3: Done getting dog pics!");
+    } catch(err){
+        console.log("ERROR");
+    }
+})();
+
+/*
+console.log("1: Will get dog pics!");
+getDogPic()
+    .then((x) => {
+        console.log(x);
+        console.log("3: Done getting dog pics!");
+    })
+    .catch((err) => {
+        console.log("ERROR");
+    });
+*/
 
 // readFilePro(`${__dirname}/dog.txt`)
 //     .then((data) => {
