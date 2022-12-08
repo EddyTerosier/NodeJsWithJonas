df895c5b501fc0d79917095c8b476f10e8cc10f1
Eddy <eddyjohn.terosier@hotmail.com>
2022-12-08 16:29:41 +0100

    Start/end CallBack Hell

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
         .end((err, res) => {
-            console.log(res.body);
+            if(err) return console.log(err.message);
+            console.log(res.body.message);
+
+            fs.writeFile("dog-image.txt", res.body.message, err =>{
+                console.log("Random dog image saved");
+            })
         });
 });
